f6aac5a42cc53ea4781ffeb2c2d9216d66a98244
HyungJin Ryu <115179487+hjryu98@users.noreply.github.com>
2023-09-05 17:41:26 +0900

    Add files via upload

---
+++ 2023_KAKAO_BLIND_RECRUITMENT/택배 배달과 수거하기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
+    long long answer = 0;
+    int d = 0, p = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        int cnt = 0;
+
+        d -= deliveries[i];
+        p -= pickups[i];
+
+        while (d < 0 || p < 0) {
+            d += cap;
+            p += cap;
+            cnt++;
+        }
+        answer += 2 * (i + 1) * cnt;
+    }
+
+    return answer;
+}
